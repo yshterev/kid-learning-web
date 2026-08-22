9f1c58955cdde8b1566b0b25da0320909f95410e
Martin Shterev <mshterev15@gmail.com>
2026-08-22 13:16:50 +0300

    refactor

--- projects/07-discover-plovdiv/wikipedia.js
+++ projects/07-discover-plovdiv/wikipedia.js
@@ -1,10 +1,14 @@
+const wikipediaContent = document.getElementById("wikipediaContent");
+const wikipediaImage = document.getElementById("wikipediaImage");
+const wikipediaLink = document.getElementById("wikipediaLink");
+
 fetch("https://en.wikipedia.org/api/rest_v1/page/summary/Plovdiv")
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        document.getElementById("wikipediaContent").textContent = data.extract;
-        document.getElementById("wikipediaImage").src = data.thumbnail.source;
-        document.getElementById("wikipediaLink").href = data.content_urls.desktop.page;
+        wikipediaContent.textContent = data.extract;
+        wikipediaImage.src = data.thumbnail.source;
+        wikipediaLink.href = data.content_urls.desktop.page;
     })
     .catch(error => {
         console.error("Error:", error);
